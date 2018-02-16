f8852bb6c818b5de948e6182c395e10306f95f34
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-02-16 18:23:09 -0500

    Try again to fix the virtualenv

---
+++ patch_virtualenv.py
@@ -0,0 +1,10 @@
+import os
+import pathlib
+import shutil
+import sys
+
+
+if 'win32' in sys.platform:
+    source = pathlib.Path(os.environ['%PYTHON%']) / 'python3.dll'
+    destination = pathlib.Path(os.environ['%VENV%']) / 'Scripts'
+    shutil.copy(source, destination)
