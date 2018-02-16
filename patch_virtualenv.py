571ae63e7d587cd6fc2e47826e1066c5f4b9a83b
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-02-16 18:50:07 -0500

    Try again to fix the virtualenv

--- patch_virtualenv.py
+++ patch_virtualenv.py
@@ -7,4 +7,5 @@ import sys
 if 'win32' in sys.platform:
     source = pathlib.Path(os.environ['PYTHON']) / 'python3.dll'
     destination = pathlib.Path(os.environ['VENV']) / 'Scripts'
-    shutil.copy(source, destination)
+    # str() required for python 3.5  :[
+    shutil.copy(str(source), str(destination))
