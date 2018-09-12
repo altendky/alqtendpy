1681e56843d95e2fb3d368570f89d9615baa6892
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-09-11 23:14:59 -0400

    Add asyncCallbacks and use for Twisted tests
    https://github.com/pytest-dev/pytest-twisted/issues/31

--- src/altendpyqt5/tests/test_twisted.py
+++ src/altendpyqt5/tests/test_twisted.py
@@ -1,3 +1,4 @@
+import decorator
 from PyQt5 import QtCore
 import pytest
 import twisted
@@ -7,6 +8,16 @@ import altendpyqt5.twisted
 
 
 pytestmark = pytest.mark.twisted
+
+
+# https://github.com/pytest-dev/pytest-twisted/issues/31
+@decorator.decorator
+def asyncCallbacks(f, *args, **kwargs):
+    @pytest.inlineCallbacks
+    def wrapper(*args, **kwargs):
+        yield twisted.internet.defer.ensureDeferred(f())
+
+    return wrapper
 
 
 @pytest.inlineCallbacks
@@ -28,18 +39,15 @@ def test_yield_for_signal_arguments(qtbot):
     assert result == source.args
 
 
-async def async_await_for_signal():
+@asyncCallbacks
+async def test_await_for_signal():
     timer = altendpyqt5.tests.utils.singleshot_immediate_timer()
 
     await altendpyqt5.twisted.signal_as_deferred(timer.timeout)
 
 
-@pytest.inlineCallbacks
-def test_await_for_signal():
-    yield twisted.internet.defer.ensureDeferred(async_await_for_signal())
-
-
-async def async_await_for_signal_result():
+@asyncCallbacks
+async def test_await_for_signal_result():
     source = altendpyqt5.tests.utils.Source(args=('hi', 42))
 
     timer = altendpyqt5.tests.utils.singleshot_immediate_timer()
@@ -50,14 +58,8 @@ async def async_await_for_signal_result():
     assert result == source.args
 
 
-@pytest.inlineCallbacks
-def test_await_for_signal_result():
-    yield twisted.internet.defer.ensureDeferred(
-        async_await_for_signal_result(),
-    )
-
-
-async def async_await_immediate_signal_manual():
+@asyncCallbacks
+async def test_await_immediate_signal_manual():
     source = altendpyqt5.tests.utils.Source(args=('hi', 42))
 
     d = altendpyqt5.twisted.signal_as_deferred(source.signal)
@@ -67,14 +69,8 @@ async def async_await_immediate_signal_manual():
     assert result == source.args
 
 
-@pytest.inlineCallbacks
-def test_async_await_immediate_signal_manual():
-    yield twisted.internet.defer.ensureDeferred(
-        async_await_immediate_signal_manual(),
-    )
-
-
-async def async_await_immediate_signal_integrated():
+@asyncCallbacks
+async def test_await_immediate_signal_integrated():
     source = altendpyqt5.tests.utils.Source(args=('hi', 42))
 
     result = await altendpyqt5.twisted.signal_as_deferred(
@@ -83,10 +79,3 @@ async def async_await_immediate_signal_integrated():
     )
 
     assert result == source.args
-
-
-@pytest.inlineCallbacks
-def test_async_await_immediate_signal_integrated():
-    yield twisted.internet.defer.ensureDeferred(
-        async_await_immediate_signal_integrated(),
-    )
